e33ed8dac5e034c4832d83e38fe1ca0b9e2b0fa3
HuskyHsu <chessman416@hotmail.com>
2018-06-02 23:27:30 +0800

    第一版完成

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
+var url = 'https://script.google.com/macros/s/AKfycbyOkCaKC-q75jN8NPx4oxLvkcIyEJLDGZDKUuAZ_Rl9JufGr1Uf/exec';
+
 var map = L.map('map').fitWorld();
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets'
+L.tileLayer('https://mt{s}.google.com/vt/x={x}&y={y}&z={z}&hl=zh-TW', {
+    id: 'streets',
+    subdomains: "012",
+    // attribution: 'Map data: &copy; Google'
 }).addTo(map);
+
+var latlng = "";
 
 function onLocationFound(e) {
     var radius = e.accuracy / 2;
 
     // L.marker(e.latlng).addTo(map)
     //     .bindPopup("You are within " + radius + " meters from this point").openPopup();
-
+    latlng = e.latlng;
     L.circle(e.latlng, radius).addTo(map);
 }
 
@@ -25,3 +27,69 @@ map.on('locationfound', onLocationFound);
 map.on('locationerror', onLocationError);
 
 map.locate({setView: true, maxZoom: 16});
+
+var task_icon = {};
+
+//取得圖資
+axios({
+    method: 'get',
+    url: url + '?method=get_tasks',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+}).then(function (response) {
+
+    var mapData = response.data;
+
+    mapData.forEach(function (item) {
+        task_icon[item] = L.icon({
+            iconUrl: './img/' + item + '_.png',
+        
+            iconSize:     [48, 48], // size of the icon
+            iconAnchor:   [24, 24], // point of the icon which will correspond to marker's location
+            popupAnchor:  [0, -18] // point from which the popup should open relative to the iconAnchor
+        });
+    });
+
+});
+
+//取得圖資
+axios({
+    method: 'get',
+    url: url + '?method=get_existing_data',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+}).then(function (response) {
+
+    var mapData = response.data;
+
+    mapData.forEach(function (element) {
+        task = element.task.split('：')[1];
+
+        var googleNavigation = navigation(element.lat + ',' + element.lng, latlng.lat + ',' + latlng.lng);
+
+        var show_msg = "<div><h3>補給站名稱：" + element.site_name + '</h3><img src="' + element.image + '" width="200">' + '<br><a href=' + googleNavigation + ' target="_blank" style="font-size: 1.5em;">google\u5C0E\u822A</a>' + "</div>";
+
+        L.marker([element.lat, element.lng], {icon: task_icon[task]})
+            .addTo(map)
+            .bindPopup(show_msg);
+    });
+
+});
+
+//導航連結
+function navigation(LngLat, GPSLocation) {
+    if (navigator.userAgent.match(/android/i)) {
+        return "google.navigation:q=" + LngLat + "&mode=d";
+    } else if (GPSLocation == 'undefined,undefined') {
+        return "http://maps.google.com?q=" + LngLat;
+    } else {
+        if (navigator.userAgent.match(/(iphone|ipod|ipad);?/i)) {
+            return "comgooglemaps://?saddr=&daddr=" + LngLat + "&directionsmode=Driving&zoom=15";
+        } else {
+            return 'https://www.google.com.tw/maps/dir/' + LngLat + '/' + GPSLocation + '/@24,120.5,10z/data=!3m1!4b1!4m2!4m1!3e0';
+        }
+    };
+    return "";
+}
